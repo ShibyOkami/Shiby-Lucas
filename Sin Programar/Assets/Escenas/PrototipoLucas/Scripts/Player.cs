0524503c7b9c80a2c9089aebd689935039bdd4d1
ShibyOkami <ShibyOkami@users.noreply.github.com>
2020-05-06 18:00:54 -0300

    Prototipo-TrabajoLucas
    Mini juego bolita agarrar rings

---
+++ Sin Programar/Assets/Escenas/PrototipoLucas/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float velocidad;
+    private Rigidbody rb;
+    private int count;
+    public Text ContadorTexto;
+    public Text VictoriaTexto;
+    public AudioClip RingSonido;
+
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetContadorTexto();
+        VictoriaTexto.text = "";
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * velocidad);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coleccionable"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetContadorTexto();
+            AudioSource.PlayClipAtPoint(RingSonido, transform.position);
+        }
+    }
+    void SetContadorTexto()
+    {
+        ContadorTexto.text = "Rings: " + count.ToString();
+        if (count >=12)
+        {
+            VictoriaTexto.text = "¡Ganaste!";
+        }
+    }
+}
---
+++ Sin Programar/Assets/Escenas/PrototipoLucas/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+    }
+}
